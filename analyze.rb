dbbfbca74ce3dbee07e171c95a42cad460c7bf1d
Bobby Gerace <rfgerace@gmail.com>
2025-07-23 13:32:15 -0400

    Finish analysis

--- analyze.rb
+++ analyze.rb
@@ -1,90 +1,168 @@
-require 'set'
+require 'csv'
 
-class Words
-  attr_reader :word_list
+# The goal here is to find potential letter combinations to be used in Spelling Bee games.
+# We can leverage a scrabble dictionary to do this analysis. We'd like every game to have 
+# a pangram, so we start by finding all the words which have seven unique letters to use 
+# as candidates. We also need to make sure that there are enough potential words to find,
+# and what the max possible score is. We also need to do this for each possible choice of 
+# the central letter.
+#
+# Unfortunately, this computation would take several days if done the brute-force way.
+# Here's a faster way to do it:
+# - We only need to know the *unique* letters of a word to find out if it's valid in a given game.
+# - We can represent the "fingerprint" of a word this way, so that similar words can be combined
+#   - E.g. APPLE and LEAP both become AELP
+# - We can use these fingerprints as keys in a hashmap, where the values are Scores
+#   - A Score object just tracks the number of words and total possible score for a fingerprint
+# - Once we have a list of 7-letter combination candidates (Combos), we can figure out
+#   all possible fingerprints and check then against the hash
+#   - Crucially, there can only be 127 of these (2^7 - 1 for the empty string)
+#   - These scores can be added up for each of the 7 central letter choices where they are valid
+#
+# This approach reduces the time complexity from O(n^2) to O(n) and takes seconds instead of days
 
-  def initialize()
-    @word_list = get_word_list
+# Class that runs the algorithm described above
+class Analyzer
+  def initialize(dict_file, out_file, min_words = 50, max_words = 100)
+    @dict_file = dict_file
+    @out_file = out_file
+    @min_words = min_words
+    @max_words = max_words
   end
 
-  def get_word_list
-    File.open('english.txt').each_line.map do |line|
-      line.strip.upcase
-    end.select { |word| word.size >= 4 && word =~ /\A[A-Z]*\z/}
+  def run
+    start = Time.now
+
+    precompute_fingerprints_and_combos!
+    compute_combo_stats!
+    write_to_csv!
+
+    puts "Finished in #{Time.now - start}s"
   end
 
-  def seven_letter_combos
-    combos = Set.new
+  def precompute_fingerprints_and_combos!
+    @fingerprint_map = Hash.new(Score.new(0, 0))
+    combo_map = {}
 
-    @word_list.each do |word|
-      uniq = word.chars.uniq
+    File.open(@dict_file).each_line.map do |line|
+      word = Word.new(line)
 
-      next if uniq.size != 7
+      next unless word.valid?
 
-      combos.add(uniq.sort.join)
+      @fingerprint_map[word.fingerprint] += word.score
+      combo_map[word.fingerprint] = Combo.new(word.fingerprint) if word.fingerprint.size == 7
     end
 
-    combos
+    @combos = combo_map.values
   end
 
-  def possible_stats(combo, center_letter)
-    word_count = 0
-    possible_score = 0
+  def compute_combo_stats!
+    @combos.each do |combo|
+      combo.possible_fingerprints.each do |fingerprint|
+        fingerprint_score = @fingerprint_map[fingerprint]
 
-    letters = Letters.new(combo)
-    @word_list.each do |word|
-      next unless letters.can_spell_word?(word, center_letter)
+        fingerprint.chars.each do |central_letter|
+          combo.central_letter_scores[central_letter] += fingerprint_score
+        end
+      end
+    end
+  end
 
-      word_count +=1
-      possible_score += score(word)
+  def pretty_print
+    @combos.first(10).each do |combo|
+      combo.central_letter_scores.each do |central_letter, score|
+        puts "#{combo.fingerprint} | #{central_letter} | #{score.words} | #{score.points}"
+      end
+    end
+  end
+
+  def write_to_csv!
+    valid_count = 0
+
+    CSV.open(@out_file, 'wb') do |csv|
+      csv << ['Letters', 'Central Letter', 'Total Words', 'Total Points']
+
+      @combos.each do |combo|
+        combo.central_letter_scores.each do |central_letter, score|
+          next unless score.words >= @min_words && score.words <= @max_words
+          csv << [combo.fingerprint, central_letter, score.words, score.points]
+
+          valid_count +=1
+        end
+      end
     end
 
-    { word_count: word_count, possible_score: possible_score }
-  end
-
-  def score(word)
-    return 14 if word.size == 7
-
-    return 1 if word.size == 4
-
-    return word.size
+    puts "Combo stats written to #{@out_file} - #{valid_count} rows"
   end
 end
 
-class Letters
-  def initialize(letters)
-    @letters = letters.chars
-    @letters_set = @letters.to_set
+
+# Represents a dictionary word
+class Word
+  def initialize(word)
+    @chars = word.strip.upcase.chars
   end
 
-  def can_spell_word?(word, center_letter)
-    return false unless word.include?(center_letter)
+  def valid?
+    @chars.size >= 4 && fingerprint.size <= 7 && fingerprint =~ /\A[A-Z]*\z/
+  end
 
-    word.chars.all? { |ch| @letters_set.include?(ch) }
+  def fingerprint
+    @fingerprint ||= @chars.uniq.sort.join
+  end
+
+  def score
+    points = @chars.size == 4 ? 1 : @chars.size
+    points += 7 if fingerprint.size == 7
+
+    Score.new(1, points)
+  end
+
+  def to_s
+    @chars.join
   end
 end
 
-words = Words.new
+# Represents a 7-letter combination as a candidate for a Spelling Bee game.
+# Tracks possible word count and points separately for each choice of a central letter
+class Combo
+  attr_reader :fingerprint, :central_letter_scores
 
-combos = words.seven_letter_combos
+  def initialize(fingerprint)
+    raise "Not seven unique characters" unless fingerprint.size == 7
 
-puts combos.first(10)
-puts combos.size
+    @fingerprint = fingerprint
+    @central_letter_scores = Hash.new(Score.new(0,0))
+  end
 
-start = Time.now
-combos.first(10).each do |letters|
-  letters.chars.each do |center_letter|
-    puts center_letter + " | " + letters
-    puts words.possible_stats(letters, center_letter)
+  def possible_fingerprints(chars = fingerprint, prefix = '')
+    if chars.empty? && prefix.empty?
+      []
+    elsif chars.empty?
+      [prefix]
+    else
+      next_char = chars[0]
+      rest = chars[1..-1]
+
+      possible_fingerprints(rest, prefix) + possible_fingerprints(rest, prefix + next_char)
+    end
   end
 end
-puts "time: " + (Time.now - start).to_s
 
-puts words.word_list.size
+# Convenience object for tracking words and points so they can be easily added
+class Score
+  attr_reader :words, :points
+
+  def initialize(words, points)
+    @words = words
+    @points = points
+  end
+
+  def +(other) 
+    Score.new(other.words + words, other.points + points)
+  end
+end
+
+Analyzer.new('english.txt', 'combo_stats.csv').run
 
 
-# letters = 'HBCEIOT'
-# letters.chars.each do |center_letter|
-#   puts center_letter + " | " + letters
-#   puts words.possible_stats(letters, center_letter)
-# end
